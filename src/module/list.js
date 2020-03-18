d5576e3a72e4295a86a81c151bbe274c6390c24b
KaioWeb <kaio.webdesigner@gmail.com>
2020-03-18 20:37:34 -0300

    arquivos iniciais

---
+++ src/module/list.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import axios from 'axios';
+import {Link} from "react-router-dom";
+
+class listComponent extends React.Component  {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      listEmployees:[]
+    }
+  }
+
+  componentDidMount(){
+    const url = "http://localhost:3000/employee/list";
+    axios.get(url)
+    .then( res => { 
+      if(res.data.success){
+        const data = res.data.data
+        this.setState({listEmployees:data})
+      }
+      else{
+        alert("Error web service")
+      }
+      
+    })
+    .catch(error => {
+      alert("Error server "+error)
+    })
+
+    
+
+  }
+
+  render()
+  {
+    return (
+      <table class="table table-hover table-striped">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Role</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Phone</th>
+            <th colspan="2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+          {this.loadFillData()}
+        </tbody>
+      </table>
+    );
+  }
+  loadFillData(){
+    return this.state.listEmployees.map((data)=>{
+      return (
+        <tr>
+            <th>{data.id}</th>
+            <td>{data.role.role}</td>
+            <td>{data.name}</td>
+            <td>{data.email}</td>
+            <td>{data.address}</td>
+            <td>{data.phone}</td>
+            <td>
+              
+              <Link class="btn btn-outline-info" to={"/edit/"+data.id}>Edit</Link>
+            </td>
+            <td>
+              <Link class="btn btn-outline-danger "> Delete </Link>
+            </td>
+          </tr>
+      )
+    })
+  }
+
+}
+
+export default listComponent;
